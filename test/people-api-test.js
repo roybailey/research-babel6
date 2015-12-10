80acb16cac07df3064b94194763ed2c89a7f5caa
Roy Bailey <roybaileybiz@gmail.com>
2015-12-10 08:16:47 +0000

    mocha test updates

--- test/people-api-test.js
+++ test/people-api-test.js
@@ -7,21 +7,34 @@ var supertest = require('supertest');
 var api = supertest('http://localhost:3000');
 
 var logRequest = function (req) {
-    console.log("> "+req);
+    console.log("> " + req);
     return req;
 };
 var logResponse = function (res) {
-    console.log("< "+res.status);
+    console.log("< " + res.status);
     //console.log(JSON.stringify(res.headers));
     console.log(res.body);
 };
 
+var jsonSchema = {
+    id: 'number',
+    name: 'String',
+    location: 'String'
+};
 var jsonInput = {
-    name: "mocha"
+    name: 'mocha'
+};
+var jsonPatch = {
+    name: 'mocha',
+    location: 'UK'
+};
+var jsonUpdate = {
+    name: 'chai',
+    location: 'USA'
 };
 
 describe('POST /people', function () {
-    it('should create new record', function (done) {
+    it('should create new resource record from input data', function (done) {
 
         api.post(logRequest('/people'))
             .set('Accept', 'application/json')
@@ -30,9 +43,13 @@ describe('POST /people', function () {
             .end(function (err, res) {
                 if (err) return done(err);
                 logResponse(res);
-                expect(res.body.id).to.be.a('number');
-                expect(res.body.name).to.be.a('string');
-                expect(res.body.name).to.equal(jsonInput.name);
+
+                (res.body).should.contain.keys(['id', 'name']);
+                (res.body.id).should.be.a(jsonSchema.id);
+                (res.body.name).should.be.a(jsonSchema.name);
+                (res.body.name).should.equal(jsonInput.name);
+                (res.body).should.contain(jsonInput);
+
                 jsonInput.id = res.body.id;
                 done();
             });
@@ -41,7 +58,7 @@ describe('POST /people', function () {
 });
 
 describe('GET /people/{id}', function () {
-    it('should respond with json', function (done) {
+    it('should find resource and respond with json', function (done) {
 
         api.get(logRequest('/people/' + jsonInput.id))
             .set('Accept', 'application/json')
@@ -49,6 +66,13 @@ describe('GET /people/{id}', function () {
             .end(function (err, res) {
                 if (err) return done(err);
                 logResponse(res);
+
+                (res.body).should.contain.keys(['id', 'name']);
+                (res.body.id).should.be.a(jsonSchema.id);
+                (res.body.name).should.be.a(jsonSchema.name);
+                (res.body.name).should.equal(jsonInput.name);
+                (res.body).should.contain(jsonInput);
+
                 done();
             });
 
@@ -56,7 +80,7 @@ describe('GET /people/{id}', function () {
 });
 
 describe('GET /people', function () {
-    it('should respond with json', function (done) {
+    it('should find all resources and respond with json array', function (done) {
 
         api.get(logRequest('/people'))
             .set('Accept', 'application/json')
@@ -64,11 +88,98 @@ describe('GET /people', function () {
             .end(function (err, res) {
                 if (err) return done(err);
                 logResponse(res);
-                expect(res.body).to.be.instanceof(Array);
-                res.body.should.contain.an.item.with.property('name', 'mocha');
+
+                (res.body).should.be.instanceof(Array);
+                (res.body).should.contain.an.item(jsonInput);
+                (res.body).should.contain.an.item.with.property('id', jsonInput.id);
+                (res.body).should.contain.an.item.with.property('name', jsonInput.name);
+
                 done();
             });
 
     });
 });
 
+describe('PATCH /people/{id}', function () {
+
+    it('should perform partial update of record', function (done) {
+
+        jsonPatch.id = jsonInput.id;
+        api.patch(logRequest('/people/' + jsonInput.id))
+            .send(jsonPatch)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+                if (err) return done(err);
+                logResponse(res);
+
+                (res.body).should.contain.keys(['id', 'name', 'location']);
+                (res.body.id).should.be.a(jsonSchema.id);
+                (res.body.name).should.be.a(jsonSchema.name);
+                (res.body.name).should.equal(jsonInput.name);
+                (res.body.location).should.equal(jsonPatch.location);
+                (res.body).should.contain(jsonPatch);
+
+                done();
+            });
+
+    });
+
+});
+
+describe('PUT /people/{id}', function () {
+
+    it('should perform full replacement of record', function (done) {
+
+        jsonUpdate.id = jsonInput.id;
+        api.put(logRequest('/people/' + jsonInput.id))
+            .send(jsonUpdate)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+                if (err) return done(err);
+                logResponse(res);
+
+                (res.body).should.contain.keys(['id', 'name', 'location']);
+                (res.body.id).should.be.a(jsonSchema.id);
+                (res.body.name).should.be.a(jsonSchema.name);
+                (res.body.id).should.equal(jsonUpdate.id);
+                (res.body.name).should.equal(jsonUpdate.name);
+                (res.body.location).should.equal(jsonUpdate.location);
+                (res.body).should.contain(jsonUpdate);
+
+                done();
+            });
+
+    });
+
+});
+
+describe('DELETE /people/{id}', function () {
+
+    it('should delete record', function (done) {
+
+        api.delete(logRequest('/people/' + jsonInput.id))
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+                if (err) return done(err);
+                logResponse(res);
+                done();
+            });
+
+    });
+
+    it('should no longer find the record', function (done) {
+
+        api.get(logRequest('/people/' + jsonInput.id))
+            .set('Accept', 'application/json')
+            .expect(204)
+            .end(function (err, res) {
+                if (err) return done(err);
+                logResponse(res);
+                done();
+            });
+
+    });
+});
