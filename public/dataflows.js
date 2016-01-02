f513993e9511ac1bbe30bf1a4f7dcd8aad063987
roybailey <roybaileybiz@gmail.com>
2016-01-02 11:09:13 +0000

    dataflows update

--- public/dataflows.js
+++ public/dataflows.js
@@ -13,6 +13,7 @@ $(function () { // on dom ready
                 'width': 'label',
                 'height': 'label',
                 'content': 'data(name)',
+                'text-wrap': 'wrap',
                 'text-valign': 'center',
                 'text-outline-width': 2,
                 'text-outline-color': 'data(faveColor)',
@@ -54,11 +55,11 @@ $(function () { // on dom ready
 
         elements: {
             nodes: [
-                {data: {id: 'atb', name: 'analytics-toolbox', weight: 65, faveColor: '#E0A2A2', faveShape: 'ellipse'}},
-                {data: {id: 'alcds', name: 'analytics-lcds-adaptor', weight: 45, faveColor: '#F5A45D', faveShape: 'octagon'}},
-                {data: {id: 'atbmp', name: 'analytics-toolbox-message-processor', weight: 45, faveColor: 'red', faveShape: 'rectangle'}},
-                {data: {id: 'adp', name: 'analytics-data-provider', weight: 75, faveColor: 'red', faveShape: 'rectangle'}},
-                {data: {id: 'ars', name: 'analytics-restful-service', weight: 70, faveColor: 'green', faveShape: 'roundrectangle'}}
+                {data: {id: 'atb', name: 'analytics\ntoolbox', weight: 65, faveColor: '#E0A2A2', faveShape: 'ellipse'}},
+                {data: {id: 'alcds', name: 'analytics\nlcds\nadaptor', weight: 45, faveColor: '#F5A45D', faveShape: 'octagon'}},
+                {data: {id: 'atbmp', name: 'analytics\ntoolbox\nmessage\nprocessor', weight: 45, faveColor: 'red', faveShape: 'rectangle'}},
+                {data: {id: 'adp', name: 'analytics\ndata\nprovider', weight: 75, faveColor: 'red', faveShape: 'rectangle'}},
+                {data: {id: 'ars', name: 'analytics\nrestful\nservice', weight: 70, faveColor: 'green', faveShape: 'roundrectangle'}}
             ],
             edges: [
                 {data: {source: 'atb', target: 'alcds', faveColor: 'red', strength: 90, label: '29west'}},
